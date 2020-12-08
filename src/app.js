82eb4201ba4ee84afbfacfbd2659fc908d5c8bc0
Fabrus98 <52037035+Fabrus98@users.noreply.github.com>
2020-12-09 00:14:53 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use( cors( {
-    origin: [ "https://italianhubot-dashboard-backend.herokuapp.com:3001" ],
+    origin: [ "https://italianhubot-dashboard-backend.herokuapp.com" ],
     credentials: true,
 }))
 
